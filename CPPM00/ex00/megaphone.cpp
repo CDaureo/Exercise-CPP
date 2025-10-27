8db9611ad3c7124fc79ec25283c7e10f937420d8
CDaureo <cdaureo08@gmail.com>
2025-10-27 14:19:58 +0100

    Done ex00

---
+++ CPPM00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdaureo- <cdaureo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 13:27:23 by cdaureo-          #+#    #+#             */
+/*   Updated: 2025/10/27 14:09:03 by cdaureo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream> // Permite el uso de std::cout, std::endl
+#include <string>
+#include <cctype>
+int main(int argc, char **argv) {
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		char c = '\0';
+		for (size_t j = 0; argv[i][j]; j++)
+		{
+			c = std::toupper(argv[i][j]);
+			std::cout << c;
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
---
+++ CPPM00/ex01/Contact.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+class Contacto {
+private:
+    std::string nombre;
+    int telefono;
+public:
+    Contacto(); // Constructor
+    ~Contacto(); // Destructor
+};
