96f2ad56f1e9159cb64ffcef0515865a2681c2cf
Carlos Daureo Almaraz <cdaureo-@c2r8s6.42madrid.com>
2026-01-08 12:02:18 +0100

    Edit ex00

--- CPPM00/ex00/Megaphone.cpp
+++ CPPM00/ex00/Megaphone.cpp
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cdaureo- <cdaureo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 13:27:23 by cdaureo-          #+#    #+#             */
-/*   Updated: 2025/10/27 15:04:10 by cdaureo-         ###   ########.fr       */
+/*   Updated: 2026/01/08 12:01:33 by cdaureo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream> // Permite el uso de std::cout, std::endl
 #include <string>
 #include <cctype>
-int main() {
+int main(int argc, char **argv) {
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
